6cc184b0e4d82f943d6b52663c8f55e7c6e30486
bintang <bntngputro@gmail.com>
2022-05-24 10:54:10 +0700

    change logo

--- app_pasar/resources/views/layouts/app.blade.php
+++ app_pasar/resources/views/layouts/app.blade.php
@@ -5,19 +5,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Pasar 4.0') }}</title>
     <meta name="theme-color" content="#ffffff">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 <div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
     <div class="sidebar-brand d-none d-md-flex">
-        <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
-            <use xlink:href="{{ asset('icons/brand.svg#full') }}"></use>
-        </svg>
-        <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
-            <use xlink:href="{{ asset('icons/brand.svg#signet') }}"></use>
-        </svg>
+        <img class="sidebar-brand-full" width="40" height="40" alt="CoreUI Logo"
+            src="icons/logo.png">
+        <img class="sidebar-brand-narrow" width="40" height="40" alt="CoreUI Logo"
+        src="icons/logo.png">
     </div>
     @include('layouts.navigation')
     <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
