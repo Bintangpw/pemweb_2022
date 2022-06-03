2a61667cd18e98ba280da95972a7e392145ff127
bintang <bntngputro@gmail.com>
2022-06-03 16:30:50 +0700

    db

---
+++ app_pasar.sql
@@ -0,0 +1,363 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 03, 2022 at 11:29 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `app_pasar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `konfigurasi`
+--
+
+CREATE TABLE `konfigurasi` (
+  `id_konf` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `value` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2022_04_10_064730_create_konfigurasi_table', 1),
+(6, '2022_04_10_084335_create_pemilik_table', 1),
+(7, '2022_04_10_084356_create_pasar_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasar`
+--
+
+CREATE TABLE `pasar` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kode_pasar` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_by` bigint(20) UNSIGNED DEFAULT NULL,
+  `edited_by` bigint(20) UNSIGNED DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `pasar`
+--
+
+INSERT INTO `pasar` (`id`, `nama`, `alamat`, `kode_pasar`, `created_by`, `edited_by`, `created_at`, `updated_at`) VALUES
+(1, 'Pasar Johar', 'Semarang Utara', 'PJ', NULL, NULL, '2022-06-03 00:06:23', '2022-06-03 02:07:18'),
+(4, 'Pasar Gede', 'Bandung', 'PG', NULL, NULL, '2022-06-03 01:18:44', '2022-06-03 01:18:44');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemilik`
+--
+
+CREATE TABLE `pemilik` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nik` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_wa` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `no_telp` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_by` bigint(20) UNSIGNED DEFAULT NULL,
+  `edited_by` bigint(20) UNSIGNED DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `pemilik`
+--
+
+INSERT INTO `pemilik` (`id`, `nama`, `alamat`, `nik`, `no_wa`, `no_telp`, `created_by`, `edited_by`, `created_at`, `updated_at`) VALUES
+(1, 'Bintang', 'Semarang Utara', '3506255555', '087736623997', '035-55555', NULL, NULL, '2022-06-03 00:06:46', '2022-06-03 02:15:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tenant`
+--
+
+CREATE TABLE `tenant` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_pemilik` bigint(20) UNSIGNED NOT NULL,
+  `latitude_tenant` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `longitude_tenant` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `harga_iuran` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_pasar` bigint(20) UNSIGNED NOT NULL,
+  `created_by` bigint(20) UNSIGNED DEFAULT NULL,
+  `edited_by` bigint(20) UNSIGNED DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tenant`
+--
+
+INSERT INTO `tenant` (`id`, `nama`, `id_pemilik`, `latitude_tenant`, `longitude_tenant`, `harga_iuran`, `id_pasar`, `created_by`, `edited_by`, `created_at`, `updated_at`) VALUES
+(5, 'Kios Sejahtera Abadi', 1, '12345', '12345', '250000', 1, NULL, NULL, '2022-06-03 02:16:37', '2022-06-03 02:23:06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Bintang', 'admin@admin.com', NULL, '$2y$10$ZZX89rEghq.TOc.Kcf.bu.n57PfQtEwAAORxwVfNxgmEdGpaXSsCa', NULL, '2022-06-02 21:13:38', '2022-06-02 21:13:38');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `konfigurasi`
+--
+ALTER TABLE `konfigurasi`
+  ADD PRIMARY KEY (`id_konf`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pasar`
+--
+ALTER TABLE `pasar`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pasar_created_by_foreign` (`created_by`),
+  ADD KEY `pasar_edited_by_foreign` (`edited_by`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `pemilik`
+--
+ALTER TABLE `pemilik`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pemilik_created_by_foreign` (`created_by`),
+  ADD KEY `pemilik_edited_by_foreign` (`edited_by`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `tenant`
+--
+ALTER TABLE `tenant`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tenant_created_by_foreign` (`created_by`),
+  ADD KEY `tenant_edited_by_foreign` (`edited_by`),
+  ADD KEY `tenant_id_pasar_foreign` (`id_pasar`),
+  ADD KEY `tenant_id_pemilik_foreign` (`id_pemilik`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `konfigurasi`
+--
+ALTER TABLE `konfigurasi`
+  MODIFY `id_konf` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `pasar`
+--
+ALTER TABLE `pasar`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `pemilik`
+--
+ALTER TABLE `pemilik`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tenant`
+--
+ALTER TABLE `tenant`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pasar`
+--
+ALTER TABLE `pasar`
+  ADD CONSTRAINT `pasar_created_by_foreign` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `pasar_edited_by_foreign` FOREIGN KEY (`edited_by`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `pemilik`
+--
+ALTER TABLE `pemilik`
+  ADD CONSTRAINT `pemilik_created_by_foreign` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `pemilik_edited_by_foreign` FOREIGN KEY (`edited_by`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `tenant`
+--
+ALTER TABLE `tenant`
+  ADD CONSTRAINT `tenant_created_by_foreign` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `tenant_edited_by_foreign` FOREIGN KEY (`edited_by`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `tenant_id_pasar_foreign` FOREIGN KEY (`id_pasar`) REFERENCES `pasar` (`id`),
+  ADD CONSTRAINT `tenant_id_pemilik_foreign` FOREIGN KEY (`id_pemilik`) REFERENCES `pemilik` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
