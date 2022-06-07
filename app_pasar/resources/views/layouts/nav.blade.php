36da5a4ac7d7158d873798027a387e5ab351e8bc
bintang <bntngputro@gmail.com>
2022-06-07 20:32:29 +0700

    update

--- app_pasar/resources/views/layouts/nav.blade.php
+++ app_pasar/resources/views/layouts/nav.blade.php
@@ -1,7 +1,6 @@
 <nav class="navbar navbar-dark navbar-theme-primary px-4 col-12 d-md-none">
     <a class="navbar-brand me-lg-5" href="{{ route('home') }}">
-        <img class="navbar-brand-dark" src="{{ asset('images/brand/light.svg') }}" alt="Volt logo" />
-        <img class="navbar-brand-light" src="{{ asset('images/brand/dark.svg') }}" alt="Volt logo" />
+        <img src="images/brand/logo.png" height="40" width="70" alt="Volt Logo">
     </a>
     <div class="d-flex align-items-center">
         <button class="navbar-toggler d-md-none collapsed" type="button" data-bs-toggle="collapse"
