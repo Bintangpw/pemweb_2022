f059b47c7c93eb1cbdaa478618df179c70519740
bintang <bntngputro@gmail.com>
2022-06-27 10:59:34 +0700

    minor

--- app_pasar/resources/views/welcome.blade.php
+++ app_pasar/resources/views/welcome.blade.php
@@ -25,9 +25,11 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-white btn btn-outline-primary">Home</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-white btn btn-outline-primary">Log in</a>
+                        <a href="{{ url('/guest') }}" class="text-sm text-gray-700 dark:text-white btn btn-outline-success">Home</a>
+                        
+                        <a href="{{ route('login') }}" class="ml-4 text-sm text-gray-700 dark:text-white btn btn-outline-primary">Log in</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-white btn btn-outline-warning">Register</a>
